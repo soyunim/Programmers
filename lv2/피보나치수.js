ad6bb0de231675e0f05fa193e41841b8a73bad48
soyunim <togksk123@naver.com>
2022-10-06 10:29:19 +0900

    Create 피보나치수.js

---
+++ lv2/피보나치수.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 0;
+    f1=0;
+    f2=1;
+    
+    for(let i=2; i<=n; i++){
+        answer=(f1+f2)%1234567;
+        f1=f2;
+        f2=answer;
+    }
+    
+    return answer;
+}
+
