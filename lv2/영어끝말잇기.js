de827cb58c7b5375cec98834a01bbaf1b157e649
soyunim <togksk123@naver.com>
2022-10-20 00:00:01 +0900

    Create 영어끝말잇기.js

---
+++ lv2/영어끝말잇기.js
@@ -0,0 +1,20 @@
+function solution(n, words) {
+    var answer = [0,0];
+
+    for(let i=0; i<words.length-1; i++){
+        for(let j=i+1; j<words.length; j++){
+            let last = words[j-1].split('');
+            let first = words[j].split('');
+            
+            if(words[i]==words[j] || last[last.length-1]!=first[0]){
+                let who=j%n+1;
+                let when=Math.ceil((j+1)/n);
+                answer=[who, when];
+            }
+            
+        }
+    }
+
+
+    return answer;
+}
