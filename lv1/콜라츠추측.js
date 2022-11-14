04f0e2e83e18cb08eba24d983f38a636db5346e8
soyunim <togksk123@naver.com>
2022-11-14 10:18:15 +0900

    create 콜라츠추측.js

---
+++ lv1/콜라츠추측.js
@@ -0,0 +1,19 @@
+function solution(num) {
+    var cnt = 0;
+    if(num==1){
+        return 0;
+    }
+    while(num!=1){
+        if(cnt>500){
+            return -1;
+        }
+        if(num%2==0){
+            num/=2;
+        }
+        else{
+            num=num*3+1;
+        }
+        cnt++;
+    }
+    return cnt;
+}
