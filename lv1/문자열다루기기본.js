6ed510ec7c0472a300d41bcadb749574413f3d7b
soyunim <togksk123@naver.com>
2022-11-26 13:20:06 +0900

    create 문자열다루기기본.js

---
+++ lv1/문자열다루기기본.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    var answer = true;
+    if(!(s.length==4 || s.length==6)){
+        return false;
+    }
+    for(let i=0; i<s.length; i++){
+        if(isNaN(s[i])==true){
+            return false;
+        }
+    }
+    return answer;
+}
