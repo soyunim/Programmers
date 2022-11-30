32651b1575557d2de387936b8a036669e17b7a15
soyunim <togksk123@naver.com>
2022-11-30 22:14:29 +0900

    진료순서정하기.js 수정

--- lv0/진료순서정하기.js
+++ lv0/진료순서정하기.js
@@ -1,4 +1,12 @@
 function solution(emergency) {
-    var new_e = emergency.sort(function(a,b){return b-a;});
-    return emergency.map(person => new_e.indexOf(person)+1);
+    var answer = [];
+    var copy = [...emergency];
+    
+    copy.sort(function(a,b){return b-a;});
+    console.log("sort",copy);
+    for(let i=0; i<emergency.length; i++){
+        answer.push(copy.indexOf(emergency[i])+1);
+    }
+    
+    return answer;
 }
